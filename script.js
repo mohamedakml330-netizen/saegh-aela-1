f03324f1a41ae73047c756609fc513e08b9ee70d
mohamedakml330-netizen <mohamedakml330@gmail.com>
2026-07-07 01:13:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+const API_KEY = "ضع_API_KEY_هنا";
+
+async function updateGold(){
+
+try {
+
+let response = await fetch(
+"https://www.goldapi.io/api/XAU/USD",
+{
+method:"GET",
+headers:{
+"x-access-token": API_KEY,
+"Content-Type":"application/json"
+}
+}
+);
+
+let data = await response.json();
+
+let goldUSD = data.price;
+
+// تحويل الأونصة إلى جرام
+let gram24 = goldUSD / 31.1035;
+
+// تحويل الدولار للجنيه (هنضيف مصدر الدولار بعدين)
+let dollar = 50;
+
+let price24 = gram24 * dollar;
+
+let price21 = price24 * 0.875;
+let price18 = price24 * 0.75;
+let price14 = price24 * 0.583;
+
+
+document.getElementById("g24buy").innerHTML =
+price24.toFixed(2);
+
+document.getElementById("g21buy").innerHTML =
+price21.toFixed(2);
+
+document.getElementById("g18buy").innerHTML =
+price18.toFixed(2);
+
+document.getElementById("g14buy").innerHTML =
+price14.toFixed(2);
+
+
+}
+
+catch(error){
+
+console.log("خطأ في تحميل الذهب");
+
+}
+
+}
+
+
+updateGold();
+
+setInterval(updateGold,60000);
