80c7fd00c89cd92ab91a8b2942a328fbfb20d09d
mohamedakml330-netizen <mohamedakml330@gmail.com>
2026-07-14 17:03:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-alert("script.js اشتغل");
-document.querySelector(".update").innerHTML = "تم تشغيل السكريبت";
+fetch("https://www.goldapi.io/api/XAU/EGP", {
+    headers: {
+        "x-access-token": "goldapi-395db2c868032a93dd29f6071dcd1585-io"
+    }
+})
+.then(res => {
+    alert("Status: " + res.status);
+    return res.text();
+})
+.then(data => {
+    console.log(data);
+    alert(data);
+})
+.catch(err => {
+    alert(err);
+});
